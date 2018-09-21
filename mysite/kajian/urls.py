b18e5bc11f2ed496378422b90790f591e9bc7758
dsasmita <dangs.work@gmail.com>
2018-09-21 22:28:42 +0700

    Update layout

--- mysite/kajian/urls.py
+++ mysite/kajian/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'kajian'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('kajian', views.ListView.as_view(), name='list'),
+    path('kajian-<int:pk>', views.DetailView.as_view(), name='detail'),
 ]
--- mysite/kajian/views.py
+++ mysite/kajian/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from django.views import generic
-from .models import Schedule
+from .models import Schedule, City
 
 
 class HomeView(generic.ListView):
@@ -9,6 +9,34 @@ class HomeView(generic.ListView):
 
     def get_queryset(self):
         return Schedule.objects.filter(start_from__gte=datetime.now()).order_by('start_from')[:4]
+
+class ListView(generic.ListView):
+    template_name = 'kajian/list.html'
+    context_object_name = 'schedule_list'
+
+    def get_queryset(self):
+        sch = Schedule.objects
+        # keyword = self.request.GET.get('keyword') or ''
+        city = self.request.GET.get('city') or '0'
+        if int(city) != 0:
+            sch = sch.filter(city_id=city)
+
+        sch = sch.filter(start_from__gte=datetime.now()).order_by('start_from')
+
+        return sch
+
+    def get_context_data(self, **kwargs):
+        keyword = self.request.GET.get('keyword') or ''
+        city = self.request.GET.get('city') or '0'
+
+        context = super(ListView, self).get_context_data(**kwargs)
+
+        context['title'] = 'Kajian Terbaru'
+        context['city'] = int(city)
+        context['keyword'] = keyword
+        context['cities'] = City.objects.order_by('name')
+
+        return context
 
 class DetailView(generic.DetailView):
     model = Schedule
