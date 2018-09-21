3975db0472cdfb942d58d70e5dcc68eda1ef848c
dsasmita <dangs.work@gmail.com>
2018-09-21 10:35:32 +0700

    Change to IsAuthenticatedOrReadOnly

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -9,26 +9,26 @@ from .serializers import ScheduleSerializer, CitySerializer, ProvinceSerializer,
 
 
 class ScheduleViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Schedule.objects.all().filter(start_from__gte=datetime.now()).order_by('-start_from')
     serializer_class = ScheduleSerializer
 
 class MosqueViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Mosque.objects.all()
     serializer_class = MosqueSerializer
 
 class CityViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = City.objects.all()
     serializer_class = CitySerializer
 
 class ProvinceViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Province.objects.all()
     serializer_class = ProvinceSerializer
 
 class UstadViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Ustad.objects.all()
     serializer_class = UstadSerializer
