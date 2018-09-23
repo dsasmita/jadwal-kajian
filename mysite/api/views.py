af13d6db6772be2e6ace8f4de1e6ff0950bc8e2c
dsasmita <dangs.work@gmail.com>
2018-09-23 19:19:20 +0700

    Update filtering

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -2,7 +2,9 @@
 # Create your views here.
 from datetime import datetime
 
+import self as self
 from rest_framework import viewsets, permissions
+from url_filter.integrations.drf import DjangoFilterBackend
 
 from kajian.models import Schedule, City, Province, Mosque, Ustad
 from .serializers import ScheduleSerializer, CitySerializer, ProvinceSerializer, MosqueSerializer, UstadSerializer
@@ -10,13 +12,24 @@ from .serializers import ScheduleSerializer, CitySerializer, ProvinceSerializer,
 
 class ScheduleViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    queryset = Schedule.objects.all().filter(start_from__gte=datetime.now()).order_by('-start_from')
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['title', 'ustad', 'mosque', 'city','start_from']
     serializer_class = ScheduleSerializer
+    queryset = Schedule.objects.all()
+
+    def get_queryset(self):
+        type = self.request.query_params.get('type', None)
+        if type is None:
+            queryset = Schedule.objects.all().filter(start_from__gte=datetime.now()).order_by('-start_from')
+        elif type == 'archive':
+            queryset = Schedule.objects.all().order_by('start_from')
+        return queryset
 
 class MosqueViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Mosque.objects.all()
     serializer_class = MosqueSerializer
+    filter_fields = ['city']
 
 class CityViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
